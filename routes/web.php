0b3b21918a28f3dbf04f9823fd3b9c4ce50b8961
Noor1236 <slaveofnoor@gmail.com>
2023-11-30 19:08:44 +0500

    todo

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
